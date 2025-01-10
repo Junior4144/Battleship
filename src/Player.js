d73a8095059aed8afc474c8c2c10a371b3104b79
Junior4144 <94208651+Junior4144@users.noreply.github.com>
2025-01-09 20:43:06 -0600

    feat: Player and dom module added, complete logic

---
+++ src/Player.js
@@ -0,0 +1,13 @@
+
+import GameBoard from "./Gameboard";
+import {leftPlayerGrid, rightPlayerGrid, body} from './dom.js'
+
+
+function realPlayer(){
+
+}
+
+function AIPlayer() {
+
+}
+
---
+++ src/dom.js
@@ -0,0 +1,17 @@
+
+
+export function body(){
+    const body = document.body;
+    return body;
+}
+
+export function leftPlayerGrid(){
+    const LP = document.querySelector('.left-player');
+    return LP
+}
+
+export function rightPlayerGrid(){
+    const RP = document.querySelector('.right-player');
+    return RP
+}
+
